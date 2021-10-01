c6e4eff0ce1c497415a34a7606efe138166ae129
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-01 11:14:11 +0530

    Update __main__.py

--- main_startup/__main__.py
+++ main_startup/__main__.py
@@ -20,8 +20,7 @@ from main_startup import (
     Friday3,
     Friday4,
     bot,
-    stacy,
-   
+    Stacy,
     friday_version,
     mongo_client,
 )
@@ -130,4 +129,4 @@ You Can Visit @FridaySupportOfficial For Updates And @FridayChat For Any Query /
 
 if __name__ == "__main__":
     Friday.loop.run_until_complete(run_bot())
-    stacy.start()
+    Stacy.start()
